11cc6a4616edea39a83d6db91e0be4e42f1a1170
Pachimarik <sahok_v@mail.ru>
2024-07-01 23:08:52 +0500

    try number 1

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
@@ -5,8 +5,9 @@ import androidx.annotation.NonNull;
 import java.util.List;
 
 import ru.yandex.practicum.contacts.model.ContactType;
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 
-public class ContactUi {
+public class ContactUi implements ListDiffInterface<ContactUi> {
 
     private final String name;
     private final String phone;
@@ -27,6 +28,15 @@ public class ContactUi {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean theSameAs(ContactUi item) {
+        if(this.hashCode() == item.hashCode()){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public String getPhone() {
